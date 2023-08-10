463926be6402bb1eabb003c3cee9672385f5cfce
partytecnico <90066593+partytecnico@users.noreply.github.com>
2023-08-10 16:47:08 -0500

    Update5

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function handleSuccess(response) {
         headerDiv.remove();
     }
     document.getElementById("outputDiv").innerHTML = "<h3 style='color:green'>Status 200: Page is up!";
-    window.location.href = "https://performancemanager8.successfactors.com/xi/ui/peopleprofile/pages/index.xhtml?bplte_company=bancolombiT4&_s.crb=MS%2fYyAUfTGBOxGsPDjFDxaUa9axTHsSIsrMqNUTdSq8%3d#blockView/blockId/block5390";
+    window.location.href = "https://performancemanager8.successfactors.com/xi/ui/payroll/pages/PayStatement.xhtml?bplte_company=bancolombiT4&_s.crb=Plq1mCfJTZQJCHgjc8Sq%252b3iDu2qPpaTvMGEBIh4SJwU%253d&mashup=true&encodedJSONEvent=UHJ1ZWJhIGRlIGNvbmV4aW9u&userId=MDAwMTc1Mjk%3D&legalEntityId=00000001&eou=true";
 }
 
 function handleVPNError(response) {
