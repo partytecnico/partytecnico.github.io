551b7076362acd688ac813fc63ae28f781e39409
JUAANHEN <juaanhen@bancolombia.com.co>
2023-08-10 20:44:46 -0500

    Correction if loop

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    pingURL();
+    if (!sessionStorage.getItem('redirected')) {
+        pingURL();
+    } else {
+        removeHeaderDiv();
+    }
 });
 
 function pingURL() {
@@ -30,14 +34,21 @@ function pingURL() {
 }
 
 function handleSuccess(response) {
-    const headerDiv = document.querySelector("#container > div.sf_common_comp-Page__header > div");
-    if (headerDiv) {
-        headerDiv.remove();
-    }
+    sessionStorage.setItem('redirected', 'true');
     document.getElementById("outputDiv").innerHTML = "<h3 style='color:green'>Status 200: Page is up!";
     window.location.href = "https://performancemanager8.successfactors.com/xi/ui/payroll/pages/PayStatement.xhtml?bplte_company=bancolombiT4&_s.crb=Plq1mCfJTZQJCHgjc8Sq%252b3iDu2qPpaTvMGEBIh4SJwU%253d&mashup=true&encodedJSONEvent=UHJ1ZWJhIGRlIGNvbmV4aW9u&userId=MDAwMTc1Mjk%3D&legalEntityId=00000001&eou=true";
+    removeHeaderDiv();
 }
 
 function handleVPNError(response) {
     document.getElementById("outputDiv").innerHTML = "<h3 style='color:red'>Debes estar conectado a una VPN</h3>";
 }
+
+
+function removeHeaderDiv() {
+    const headerDiv = document.querySelector("#renderTopNavFixedWidthV12 > xweb-shellbar").shadowRoot.querySelector("div");
+    if (headerDiv) {
+        headerDiv.remove();
+    }
+}
+
